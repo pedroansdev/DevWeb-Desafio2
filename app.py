4b89be33e2686f3375045a3d95c196e3ccc0aef7
Pedro Augusto <pedroaugdev@gmail.com>
2023-04-10 14:17:23 -0300

    Fazendo as outras rotas

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask("__name__")
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+@app.route("/quemsomos")
+def quemSomos():
+    return render_template("quemsomos.html")
